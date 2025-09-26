91bcbd25c58df2eb0459c29d16d4ccd4f33f2c22
gabrielmoc <gabrielmoc2010@hotmail.com>
2025-09-26 17:17:51 -0300

    prod

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,12 +1,23 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // Produção (Render)
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false } // Render exige SSL
+  });
+} else {
+  // Desenvolvimento local
+  pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
+}
 
 module.exports = pool;
